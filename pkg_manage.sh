3f7cf071acd8921ba45aa0e5603ef44ea0dd34ef
jqsunac <wukong@bi.a.u-tokyo.ac.jp>
2016-05-07 20:44:56 +0900

    changed unit test code.

--- pkg_manage.sh
+++ pkg_manage.sh
@@ -11,7 +11,15 @@ rm -rf PyDAIR.egg-info
 rm -rf build
 rm -rf dist
 rm ./PyDAIR/test/data/test_output_*
-
+rm PyDAIR/*.pyc
+rm PyDAIR/app/*.pyc
+rm PyDAIR/io/*.pyc
+rm PyDAIR/seq/*.pyc
+rm PyDAIR/test/*.pyc
+rm PyDAIR/bin/*.pyc
+rm PyDAIR/plot/*.pyc
+rm PyDAIR/stats/*.pyc
+rm PyDAIR/utils/*.pyc
 
 
 
